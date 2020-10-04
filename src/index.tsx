2f720c209c2153b1f44e9c72ca574cd31cfd2876
Тимофей Полухин <xalvaine@gmail.com>
2020-10-04 22:43:14 +0300

    chore(app) switch basename to process.env.PUBLIC_URL

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App'
 import './index.scss'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
